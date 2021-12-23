3f5b54f28bf352863bebd01d0ad1fe2ac1eb8237
Richard Neumann <r.neumann@homeinfo.de>
2021-12-23 18:05:53 +0100

    Use decorator to inject methods

--- dcorm/expression.py
+++ dcorm/expression.py
@@ -1,8 +1,9 @@
 """Conditional expressions."""
 
 from __future__ import annotations
-from typing import Any, NamedTuple, Optional, Union
+from typing import Any, NamedTuple, Optional
 
+from dcorm.expression_functions import expression_generator
 from dcorm.operators import Operator
 from dcorm.sql import sql
 
@@ -10,120 +11,13 @@ from dcorm.sql import sql
 __all__ = ['Expression']
 
 
+@expression_generator
 class Expression(NamedTuple):
     """Conditional expression for WHERE clauses."""
 
     lhs: Any
     operator: Operator
     rhs: Optional[Any] = None   # Compensate for unary operators.
-
-    def __and__(self, other: Any) -> Union[Expression, bool]:
-        if other is True:
-            return self
-
-        if other is False:
-            return False
-
-        return type(self)(self, Operator.AND, other)
-
-    def __rand__(self, other: Any) -> Union[Expression, bool]:
-        if other is True:
-            return self
-
-        if other is False:
-            return False
-
-        return type(self)(other, Operator.AND, self)
-
-    def __or__(self, other: Any) -> Union[Expression, bool]:
-        if other is True:
-            return True
-
-        if other is False:
-            return self
-
-        return type(self)(self, Operator.OR, other)
-
-    def __ror__(self, other: Any) -> Union[Expression, bool]:
-        if other is True:
-            return True
-
-        if other is False:
-            return self
-
-        return type(self)(other, Operator.OR, self)
-
-    def __add__(self, other: Any) -> Expression:
-        return type(self)(self, Operator.ADD, other)
-
-    def __radd__(self, other: Any) -> Expression:
-        return type(self)(other, Operator.ADD, self)
-
-    def __sub__(self, other: Any) -> Expression:
-        return type(self)(self, Operator.SUB, other)
-
-    def __rsub__(self, other: Any) -> Expression:
-        return type(self)(other, Operator.SUB, self)
-
-    def __mul__(self, other: Any) -> Expression:
-        return type(self)(self, Operator.MUL, other)
-
-    def __rmul__(self, other: Any) -> Expression:
-        return type(self)(other, Operator.MUL, self)
-
-    def __div__(self, other: Any) -> Expression:
-        return type(self)(self, Operator.DIV, other)
-
-    def __rdiv__(self, other: Any) -> Expression:
-        return type(self)(other, Operator.DIV, self)
-
-    def __xor__(self, other: Any) -> Expression:
-        return type(self)(self, Operator.XOR, other)
-
-    def __rxor__(self, other: Any) -> Expression:
-        return type(self)(other, Operator.XOR, self)
-
-    def __eq__(self, other: Any) -> Expression:
-        if other is None:
-            return type(self)(self, Operator.IS, other)
-
-        return type(self)(self, Operator.EQ, other)
-
-    def __lt__(self, other: Any) -> Expression:
-        return type(self)(self, Operator.EQ, other)
-
-    def __le__(self, other: Any) -> Expression:
-        return type(self)(self, Operator.LE, other)
-
-    def __gt__(self, other: Any) -> Expression:
-        return type(self)(self, Operator.GT, other)
-
-    def __ge__(self, other: Any) -> Expression:
-        return type(self)(self, Operator.GE, other)
-
-    def __ne__(self, other: Any) -> Expression:
-        if other is None:
-            return type(self)(self, Operator.IS_NOT, other)
-
-        return type(self)(self, Operator.GE, other)
-
-    def __lshift__(self, other: Any) -> Expression:
-        return type(self)(self, Operator.IN, other)
-
-    def __rshift__(self, other: Any) -> Expression:
-        return type(self)(self, Operator.IS, other)
-
-    def __mod__(self, other: Any) -> Expression:
-        return type(self)(self, Operator.LIKE, other)
-
-    def __pow__(self, other: Any) -> Expression:
-        return type(self)(self, Operator.ILIKE, other)
-
-    def __matmul__(self, other: Any) -> Expression:
-        return type(self)(self, Operator.CONCAT, other)
-
-    def __rmatmul__(self, other: Any) -> Expression:
-        return type(self)(other, Operator.CONCAT, self)
 
     @property
     def __sql__(self) -> str:
