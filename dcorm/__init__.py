73ce7ff6e2cc3d35646a66b1e45508d3dffbeff7
Richard Neumann <r.neumann@homeinfo.de>
2021-12-23 11:15:25 +0100

    Export API objects

--- dcorm/__init__.py
+++ dcorm/__init__.py
@@ -1,1 +1,8 @@
 """A daclasses-based ORM framework for relational databases."""
+
+from dcorm.database import Database
+from dcorm.fields import field
+from dcorm.model import ModelType, Model
+
+
+__all__ = ['field', 'Database', 'Model', 'ModelType']
