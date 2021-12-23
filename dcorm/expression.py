72fef2f3a69477693c8edf48d162f9cc83c3a1b6
Richard Neumann <r.neumann@homeinfo.de>
2021-12-23 12:40:49 +0100

    [WIP] implement expression

--- dcorm/expression.py
+++ dcorm/expression.py
@@ -1,7 +1,8 @@
 """Conditional expressions."""
 
+from __future__ import annotations
 from dataclasses import dataclass
-from typing import Any
+from typing import Any, Optional, Union
 
 from dcorm.operators import Operator
 from dcorm.sql import sql
@@ -16,9 +17,48 @@ class Expression:
 
     lhs: Any
     operator: Operator
-    rhs: Any
+    rhs: Optional[Any] = None   # Compensate for unary operators.
+
+    def __and__(self, other: Any) -> Union[Expression, bool]:
+        if other is True:
+            return self
+
+        if other is False:
+            return False
+
+        return type(self)(self, Operator.AND, other)
+
+    def __rand__(self, other: Any) -> Union[Expression, bool]:
+        if other is True:
+            return self
+
+        if other is False:
+            return False
+
+        return type(self)(other, Operator.AND, self)
+
+    def __or__(self, other: Any) -> Union[Expression, bool]:
+        if other is True:
+            return True
+
+        if other is False:
+            return self
+
+        return type(self)(self, Operator.OR, other)
+
+    def __ror__(self, other: Any) -> Union[Expression, bool]:
+        if other is True:
+            return True
+
+        if other is False:
+            return self
+
+        return type(self)(other, Operator.OR, self)
 
     @property
     def __sql__(self) -> str:
         """Returns an SQL representation of the expression."""
-        return f'({sql(self.lhs)} {self.operator.__sql__} {sql(self.rhs)})'
+        if self.rhs is None:    # Compensate for unary operators.
+            return f'({sql(self.operator)} {sql(self.lhs)})'
+
+        return f'({sql(self.lhs)} {sql(self.operator)} {sql(self.rhs)})'
