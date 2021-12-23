3f389e1254558826a00f480c8a3c947bfd43f708
Richard Neumann <mail@richard-neumann.de>
2021-12-24 00:45:30 +0100

    Rename value to keyword

--- dcorm/literal.py
+++ dcorm/literal.py
@@ -9,18 +9,18 @@ __all__ = ['Literal']
 class Literal(NamedTuple):
     """An SQL literal."""
 
-    value: str
+    keyword: str
     space_left: bool = False
     space_right: bool = False
 
     def __str__(self):
         if self.space_left:
             if self.space_right:
-                return f' {self.value} '
+                return f' {self.keyword} '
 
-            return f' {self.value}'
+            return f' {self.keyword}'
 
         if self.space_right:
-            return f'{self.value} '
+            return f'{self.keyword} '
 
-        return self.value
+        return self.keyword
