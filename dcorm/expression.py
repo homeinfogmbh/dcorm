5aa52369f8fc802bc805c53e00f9b020aff850e7
Richard Neumann <mail@richard-neumann.de>
2021-12-24 02:00:54 +0100

    Rewrite Expression.__sql__

--- dcorm/expression.py
+++ dcorm/expression.py
@@ -3,9 +3,9 @@
 from __future__ import annotations
 from typing import Any, NamedTuple, Optional
 
+from dcorm.engine import Engine
 from dcorm.expression_functions import expression_generator
 from dcorm.operators import Operator
-from dcorm.sql import sql
 
 
 __all__ = ['Expression']
@@ -17,12 +17,12 @@ class Expression(NamedTuple):
 
     lhs: Any
     operator: Operator
-    rhs: Optional[Any] = None   # Compensate for unary operators.
+    rhs: Optional[Any] = None   # Allow for unary operators.
 
-    @property
-    def __sql__(self) -> str:
-        """Returns an SQL representation of the expression."""
-        if self.rhs is None:    # Compensate for unary operators.
-            return f'{sql(self.operator)} {sql(self.lhs)}'
+    def __sql__(self, engine: Engine) -> Engine:
+        engine.sql(self.lhs).literal(self.operator)
 
-        return f'({sql(self.lhs)} {sql(self.operator)} {sql(self.rhs)})'
+        if self.rhs is not None:
+            engine.sql(self.rhs)
+
+        return engine
