3c31e46962af41276191e1b000ca677bcd0e7162
Richard Neumann <mail@richard-neumann.de>
2021-12-24 02:03:31 +0100

    Require setting of engine in subclasses

--- dcorm/database.py
+++ dcorm/database.py
@@ -1,8 +1,14 @@
 """Database definitions."""
 
+from dcorm.engine import Engine
+
 
 __all__ = ['Database']
 
 
-class Database:
+class Database:     # pylint: disable=R0903
     """Base class for databases."""
+
+    def __init_subclass__(cls, *, engine: Engine):
+        """Sets the database engine."""
+        cls.engine = engine
