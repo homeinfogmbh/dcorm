ebc2d12510877e68ba31139d13f11535ca119c97
Richard Neumann <mail@richard-neumann.de>
2021-12-24 03:55:58 +0100

    Use quoting from engine

--- dcorm/path.py
+++ dcorm/path.py
@@ -18,6 +18,7 @@ class Path(list):
         return '.'.join(['%s'] *  len(self))
 
     def __sql__(self, engine: Engine) -> Engine:
-        engine._sql.append(self.template)   # pylint: disable=W0212
-        engine._values.extend(self)         # pylint: disable=W0212
+        sql = f'{engine.quote[0]}{self.template}{engine.quote[1]}'
+        engine._sql.append(sql)         # pylint: disable=W0212
+        engine._values.extend(self)     # pylint: disable=W0212
         return engine
